6e828e14bd5387f0bfd93042154d8b51d74269cc
Cologler <skyoflw@gmail.com>
2020-08-08 11:47:37 +0800

    first commit

---
+++ tests/test_storageonly.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2020~2999 - Cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+from tinydb import TinyDB
+from tinydb_sqlite import SQLiteStorage
+
+def open_memorydb():
+    return TinyDB(storage=SQLiteStorage, connection=':memory:')
+
+def test_do_notthing():
+    with open_memorydb() as db:
+        pass
+
+def test_empty():
+    with open_memorydb() as db:
+        assert db.tables() == set()
+
+def test_insert():
+    with open_memorydb() as db:
+        a = db.table('a')
+        assert db.tables() == set()
+        assert a.all() == []
+        a.insert({'b':1})
+        assert db.tables() == {'a'}
+        assert a.all() == [{'b':1}]
+
+def test_count():
+    with open_memorydb() as db:
+        a = db.table('a')
+        assert len(a) == 0
+        a.insert({'b':1})
+        assert len(a) == 1
---
+++ tinydb_sqlite/storages.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2020~2999 - Cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+from typing import *
+from collections.abc import MutableMapping
+from sqlite3 import Connection, connect, Cursor
+from contextlib import closing
+import json
+
+from tinydb.storages import Storage
+
+
+class SQLiteTableProxy(MutableMapping):
+    SQL_CREATE_TABLE = 'CREATE TABLE IF NOT EXISTS "%s" (key TEXT PRIMARY KEY, type TEXT, value BLOB);'
+    SQL_DROP_TABLE = 'DROP TABLE IF NOT EXISTS "%s";'
+    SQL_UPSERT_ITEM = 'INSERT OR REPLACE INTO %s VALUES (?, ?, ?);'
+    SQL_DELETE_ITEM = 'DELETE FROM %s WHERE key=?;'
+    SQL_ITER_KEYS = 'SELECT key FROM %s;'
+    SQL_GET_VALUE = 'SELECT type, value FROM %s WHERE key=?;'
+    SQL_COUNT = 'SELECT COUNT(*) FROM %s'
+
+    def __init__(self, connection: Connection, tablename: str):
+        super().__init__()
+        self._conn = connection
+        self._tablename = tablename
+
+    def create_table(self, cursor: Cursor):
+        cursor.execute(self.SQL_CREATE_TABLE % self._tablename)
+
+    def drop_table(self, cursor: Cursor):
+        cursor.execute(self.SQL_DROP_TABLE % self._tablename)
+
+    def overwrite(self, cursor: Cursor, data: Dict[str, Any]):
+        # remove
+        cursor.executemany(self.SQL_DELETE_ITEM % self._tablename, set(self) - set(data))
+
+        # update
+        params = []
+        for k, v in data.items():
+            params.append((k, *self._encode_value(v)))
+        cursor.executemany(self.SQL_UPSERT_ITEM % self._tablename, params)
+
+    def _decode_value(self, type_: str, value):
+        if type_ == 'null':
+            return None
+        if type_ == 'bool':
+            return bool(value)
+        if type_ == 'int':
+            return int(value)
+        if type_ == 'float':
+            return float(value)
+        if type_ == 'str':
+            return str(value)
+        if type_ == 'bytes':
+            return bytes(value)
+        if type_ == 'json':
+            return json.loads(value)
+        raise NotImplementedError
+
+    def _encode_value(self, value) -> Tuple[str, Any]:
+        if value is None:
+            return 'null', None
+        type_ = type(value)
+        if type_ in (bool, int, float, str, bytes):
+            return type_.__name__, value
+        return 'json', json.dumps(value, ensure_ascii=False)
+
+    def __getitem__(self, key):
+        with closing(self._conn.execute(self.SQL_GET_VALUE % self._tablename, (key, ))) as cursor:
+            t, v = cursor.fetchone()
+            return self._decode_value(t, v)
+
+    def __setitem__(self, key, value):
+        param = (key, *self._encode_value(value))
+        with closing(self._conn.execute(self.SQL_UPSERT_ITEM % self._tablename, param)):
+            self._conn.commit()
+
+    def __delitem__(self, key):
+        param = key,
+        with closing(self._conn.execute(self.SQL_DELETE_ITEM % self._tablename, param)):
+            self._conn.commit()
+
+    def __iter__(self):
+        with closing(self._conn.execute(self.SQL_ITER_KEYS % self._tablename)) as cursor:
+            for k, in cursor:
+                yield k
+
+    def __len__(self):
+        with closing(self._conn.execute(self.SQL_COUNT % self._tablename)) as cursor:
+            v, = cursor.fetchone()
+            return v
+
+
+class SQLiteStorage(Storage):
+    SQL_LIST_TABLES = 'SELECT name FROM sqlite_master WHERE type="table";'
+
+    def __init__(self, connection, **kwargs):
+        super().__init__()
+        if not isinstance(connection, Connection):
+            connection = connect(connection)
+        self._conn: Connection = connection
+
+    def _list_tables(self, cursor: Cursor) -> Dict[str, SQLiteTableProxy]:
+        cursor.execute(self.SQL_LIST_TABLES)
+        tablenames = [n[0] for n in cursor.fetchall()]
+        db = {}
+        for name in tablenames:
+            db[name] = SQLiteTableProxy(self._conn, name)
+        return db
+
+    def read(self) -> Optional[Dict[str, Dict[str, Any]]]:
+        with closing(self._conn.cursor()) as cursor:
+            return self._list_tables(cursor)
+
+    def write(self, data: Dict[str, Dict[str, Any]]) -> None:
+        with closing(self._conn.cursor()) as cursor:
+            tables = self._list_tables(cursor)
+            ek = set(tables)
+            nk = set(data)
+
+            # drop
+            for name in (ek - nk):
+                tables[name].drop_table(cursor)
+
+            for name in data:
+                table = tables.get(name)
+                if table is None:
+                    table = SQLiteTableProxy(self._conn, name)
+                    table.create_table(cursor)
+                table.overwrite(cursor, data[name])
+
+            self._conn.commit()
+
+    def close(self) -> None:
+        self._conn.close()
